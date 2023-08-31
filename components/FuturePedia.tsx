24686710ca40727bdd1edcb1c53783bc81063f00
Michael Ryaboy <sfr.michael@yahoo.com>
2023-08-31 14:56:46 -0700

    changes badge to official badge

--- components/FuturePedia.tsx
+++ components/FuturePedia.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const FuturePedia = () => {
   return (
     <div className="relative mt-10 m-5">
-      <div className="top-0 right-3">
-        <a
+      <div className="top-0">
+        {/* <a
           href="https://www.futurepedia.io/tool/siteexplainer"
           target="_blank"
           rel="noopener noreferrer"
@@ -19,15 +19,24 @@ const FuturePedia = () => {
               />
             </div>
             <div className="flex-grow">
-              {/* Use responsive classes to adjust font size based on screen size */}
+          
               <h3 className="mt-4 text-xl font-semibold mb-2 sm:text-xl text-lg">{"Find us on FuturePedia!"}</h3>
                 <p className="hidden sm:block text-sm text-gray-600 sm:text-sm text-xs">
-              {/* <p className="text-sm text-gray-600 sm:text-sm text-xs"> */}
+            
                 {'If you like our tool, please consider upvoting us on FuturePedia!'}
               </p>
             </div>
           </div>
-        </a>
+        </a> */}
+      <a href="https://www.futurepedia.io/tool/siteexplainer?utm_source=siteexplainer_embed" style={{ width: '250px', height: '54px' }}
+      target="_blank"
+      >
+      <img
+        src="https://www.futurepedia.io/api/image-widget?toolId=7cbff9ae-968f-4f04-b5d9-ad716f75d4e8"
+        alt="SiteExplainer | Featured on Futurepedia"
+        style={{ width: '250px', height: '54px' }}
+      />
+    </a>
       </div>
     </div>
   );
