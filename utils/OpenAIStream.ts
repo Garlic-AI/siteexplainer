36e38e54647fbcf5e24f5dfa1b9a629bb374fec8
Michael Ryaboy <sfr.michael@yahoo.com>
2023-11-13 12:34:18 -0800

    ratelimit policy

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -56,8 +56,10 @@ export async function OpenAIStream(
   // // Define the rate limiting policy
   // const rateLimitPolicy = "5;d=86400;s=ip"; // 5 requests per 86400 seconds (1 day), segmented by IP address not used
 
-  const identifier = "api";
+  const identifier = ipAddress;
   const result = await ratelimit.limit(identifier);
+
+  console.log("result", result)
 
   if (!result.success) {
     return new ReadableStream({
@@ -83,43 +85,43 @@ export async function OpenAIStream(
     body: JSON.stringify(payload),
   });
 
-  const stream = new ReadableStream({
-    async start(controller) {
-      // callback
-      function onParse(event: ParsedEvent | ReconnectInterval) {
-        if (event.type === "event") {
-          const data = event.data;
-          // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].delta?.content || "";
-            if (counter < 2 && (text.match(/\n/) || []).length) {
-              // this is a prefix character (i.e., "\n\n"), do nothing
-              return;
-            }
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-            counter++;
-          } catch (e) {
-            // maybe parse error
-            controller.error(e);
-          }
-        }
-      }
+  // const stream = new ReadableStream({
+  //   async start(controller) {
+  //     // callback
+  //     function onParse(event: ParsedEvent | ReconnectInterval) {
+  //       if (event.type === "event") {
+  //         const data = event.data;
+  //         // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
+  //         if (data === "[DONE]") {
+  //           controller.close();
+  //           return;
+  //         }
+  //         try {
+  //           const json = JSON.parse(data);
+  //           const text = json.choices[0].delta?.content || "";
+  //           if (counter < 2 && (text.match(/\n/) || []).length) {
+  //             // this is a prefix character (i.e., "\n\n"), do nothing
+  //             return;
+  //           }
+  //           const queue = encoder.encode(text);
+  //           controller.enqueue(queue);
+  //           counter++;
+  //         } catch (e) {
+  //           // maybe parse error
+  //           controller.error(e);
+  //         }
+  //       }
+  //     }
 
-      // stream response (SSE) from OpenAI may be fragmented into multiple chunks
-      // this ensures we properly read chunks and invoke an event for each SSE event stream
-      const parser = createParser(onParse);
-      // https://web.dev/streams/#asynchronous-iteration
-      for await (const chunk of res.body as any) {
-        parser.feed(decoder.decode(chunk));
-      }
-    },
-  });
+  //     // stream response (SSE) from OpenAI may be fragmented into multiple chunks
+  //     // this ensures we properly read chunks and invoke an event for each SSE event stream
+  //     const parser = createParser(onParse);
+  //     // https://web.dev/streams/#asynchronous-iteration
+  //     for await (const chunk of res.body as any) {
+  //       parser.feed(decoder.decode(chunk));
+  //     }
+  //   },
+  // });
 
-  return stream;
+  // return stream;
 }
